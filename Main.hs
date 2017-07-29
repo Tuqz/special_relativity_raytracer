07b967c539637e1156e8c5b0f089cef9e77818fb
Tuqz <jamesrawson628@gmail.com>
2017-07-29 23:51:40 +0100

    Improved commenting

--- Main.hs
+++ Main.hs
@@ -14,11 +14,14 @@ norms = map (\v -> divide v (norm v)) dirs
 rays = map rayify norms
 	where
 	rayify (Vector3 x y z) = Ray {start = (Vector4 0 (-beta) 2 0), direction = light_speed `mult` (Vector4 (-1) x y z)}
+	--the camera is located so that the closest point of the sphere appears in front of the camera
 
 intscts = map (collides (Sphere {position = (Vector4 0 0 0 0), radius = 1, velocity = (Vector4 (gamma * light_speed) (beta * gamma * light_speed) 0 0)})) rays
+--for each ray calculate the time (if applicable) to intersection with the sphere that is moving at speed beta
 
 main :: IO ()
 main = do
 	putStrLn "P2"
 	putStrLn "1001 1001 255"
 	mapM_ (\r -> if r > 0 then print . round $ (255 / (r ** 2)) else print 0) intscts
+	--convert the intersection times to colour
--- Sphere.hs
+++ Sphere.hs
@@ -10,16 +10,16 @@ data Sphere = Sphere {position :: Vector4, radius :: Double, velocity :: Vector4
 
 instance Renderable Sphere where
     collides (Sphere {position=pos, radius=r, velocity=vel}) (Ray {start=r0, direction=dir})
-        | and [cond >= 0, (d + sqrt(cond)) <= 0] = convert $ -(d + sqrt cond)/n**2
-        | cond >= 0 = convert $ (-d + sqrt cond)/n**2
+        | and [cond >= 0, (d + sqrt(cond)) <= 0] = convert $ -(d + sqrt cond)/n**2 --if the ray is outside the sphere and intersects
+        | cond >= 0 = convert $ (-d + (2 * sqrt cond))/n**2 --ray is inside the sphere
         | otherwise = -1 --Negative times are ignored, so no collision shall be -ve
         where
             convert :: Double -> Double
-            convert param = getT $ sub pos (lorentz (-v) (rel_pos `add` (param `mult` rel_dir)))
+            convert param = getT $ sub pos (lorentz (-v) (rel_pos `add` (param `mult` rel_dir))) --convert times back to reference frame times
 
-            v = light_speed * (tanh . asinh $ ((norm . getSpatial $ vel)/light_speed))
-            rel_pos = lorentz v (rotTo vel (r0 `sub` pos))
-            rel_dir = lorentz v (rotTo vel dir)
+            v = light_speed * (tanh . asinh $ ((norm . getSpatial $ vel)/light_speed)) --recover speed from velocity
+            rel_pos = rotFrom vel $ lorentz v $ rotTo vel $ r0 `sub` pos --calculate relative position of sphere in sphere's frame
+            rel_dir = rotFrom vel $ lorentz v $ rotTo vel dir --calculate direction of ray in sphere's frame
             spat_pos = getSpatial rel_pos
             spat_dir = getSpatial rel_dir
             d = dot spat_pos spat_dir
